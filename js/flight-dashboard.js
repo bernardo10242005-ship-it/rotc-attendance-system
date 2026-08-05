f27721e7b712e818e2278aeb62f407f6bb02a7fd
sir bernardo <bernardo10242005@gmail.com>
2026-08-05 17:51:52 +0800

    Update flight-dashboard.js

--- js/flight-dashboard.js
+++ js/flight-dashboard.js
@@ -1,1 +1,71 @@
+// ======================================================
+// FULL BRIGHT COLLEGE
+// ROTC ATTENDANCE MANAGEMENT SYSTEM
+// FLIGHT LEADER DASHBOARD
+// ======================================================
 
+// Load logged in Flight Leader
+const leader =
+JSON.parse(
+localStorage.getItem("flightLeader")
+);
+
+// If not logged in
+if(!leader){
+
+    window.location.href="flight-login.html";
+
+}
+
+// Display Flight Name
+document.getElementById("flightName").innerHTML=
+
+leader.flight;
+
+// ======================================================
+// VIEW ATTENDANCE
+// ======================================================
+
+function openAttendance(){
+
+    const trainingDay=
+
+    document.getElementById("trainingDay").value;
+
+    localStorage.setItem(
+
+        "selectedTrainingDay",
+
+        trainingDay
+
+    );
+
+    window.location.href=
+
+    "flight-attendance.html";
+
+}
+
+// ======================================================
+// LOGOUT
+// ======================================================
+
+function logoutFlightLeader(){
+
+    localStorage.removeItem(
+
+        "flightLeader"
+
+    );
+
+    localStorage.removeItem(
+
+        "selectedTrainingDay"
+
+    );
+
+    window.location.href=
+
+    "index.html";
+
+}
